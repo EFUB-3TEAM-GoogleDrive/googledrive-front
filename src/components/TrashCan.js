df71f3cd4097a7436c33577ca68b11838913f348
최윤재 <yunyun3599@gmail.com>
2021-11-30 20:38:01 +0900

    삭제된 파일 trashcan에서 조회

--- src/components/TrashCan.js
+++ src/components/TrashCan.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import trashcan from "../assets/trashcan.png"
 
@@ -14,18 +15,51 @@ const Warning = styled.div`
   border-radius: 10px;
 `
 
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 220px;
+  height: 150px;
+  border: 1px solid black;
+  cursor: pointer;
+  margin-right: 15px;
+  padding-top: 12px;
+  background-color: #f1f3f4;
+  align-items: center;
+  border: 1px solid #cccccc;
+  border-radius: 5px;
+`;
+
+const Image = styled.div`
+  width: 195px;
+  height: 85px;
+  background-color: white;
+  border-bottom: none;
+  box-shadow: 0 0 3px 1px #cccccc;
+  background-image: url(${(props) => props.url});
+`;
+
 const TrashCan = () => {
+  const dispatch = useDispatch();
+  const deletedRecommend = useSelector((state) => state?.recommend.trash) || [];
+  const deletedFile = useSelector(state => state?.file.trash) || [];
+  const deletedFolder = useSelector(state => state?.folder.trash) || [];
+  const deleted = deletedRecommend.concat(deletedFile).concat(deletedFolder);
   return <div>
     <Warning>
       <p>휴지통에 있는 항목이 30일 후  완전히 삭제됩니다.</p>
       <p>휴지통  비우기</p>
     </Warning>
-    <div style={{paddingTop:"10%", textAlign:"center", fontSize:"14px"}}>
-    <img style={{width:"200px"}} alt="쓰레기통" src={trashcan}/>
-    <p style={{fontSize: "18px"}}>항목 없음</p>
-    <p>휴지통으로 이동한 항목이 여기에 표시됩니다.</p>
-    <p style={{color:"#1A73e8"}}>자세히 알아보기</p>
-    </div>
+    {deleted.length === 0 ? 
+        <div style={{paddingTop:"10%", textAlign:"center", fontSize:"14px"}}>
+        <img style={{width:"200px"}} alt="쓰레기통" src={trashcan}/>
+        <p style={{fontSize: "18px"}}>항목 없음</p>
+        <p>휴지통으로 이동한 항목이 여기에 표시됩니다.</p>
+        <p style={{color:"#1A73e8"}}>자세히 알아보기</p>
+        </div>
+        :
+        <div></div>
+    }
   </div>;
 };
 
